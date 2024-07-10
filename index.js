e3241a99b8e3fd7a6aab4cf900afcdbf021157b2
Andrei Duque <solomonduque04@gmail.com>
2024-07-10 20:33:47 +0000

    working build

--- index.js
+++ index.js
@@ -18,8 +18,11 @@ const app = express();
 
 app.use(express.json());
 
-
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
 })
+
+const routes = require('./routes/routes');
+
+app.use('/api', routes)
 
