0e7a7e07cf325a61850d2bb58a8e132f161db528
Andrei Duque <solomonduque04@gmail.com>
2024-07-16 20:35:24 +0000

    renamed rte.html to createPost.html and added to previewPost.html

---
+++ frontend/scripts/previewPost_script.js
@@ -0,0 +1,4 @@
+  // this function is used to redirect the user to the createPost page
+  document.querySelector("#back").addEventListener('click', function() {
+    window.location.href = "createPost.html";
+});
