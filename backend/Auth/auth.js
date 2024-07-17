7b8556bbaa794cfdc316d510a224ee7380e7ff7a
Andrei Duque <solomonduque04@gmail.com>
2024-07-17 22:25:38 +0000

    fixed error when starting local server, commented out res.render(home) as current code has not implemented yet

--- backend/Auth/auth.js
+++ backend/Auth/auth.js
@@ -3,26 +3,6 @@ const bcrypt = require("bcryptjs")
 const jwt = require('jsonwebtoken')
 const jwtSecret ='ab4b98880feed65284bc27750ba5fe02070a9bba6b1cf28a48a54699a17db6d6b3585c'
 
-exports.adminAuth = (req, res, next) => {
-  const token = req.cookies.jwt
-  if (token) {
-    jwt.verify(token, jwtSecret, (err, decodedToken) => {
-      if (err) {
-        return res.status(401).json({ message: "Not authorized" })
-      } else {
-        if (decodedToken.role !== "admin") {
-          return res.status(401).json({ message: "Not authorized" })
-        } else {
-          next()
-        }
-      }
-    })
-  } else {
-    return res
-      .status(401)
-      .json({ message: "Not authorized, token not available" })
-  }
-}
 
 // auth.js
 exports.register = async (req, res, next) => {
@@ -170,25 +150,4 @@ exports.deleteUser = async (req, res, next) => {
         .status(400)
         .json({ message: "An error occurred", error: error.message })
     )
-}
-
-exports.userAuth = (req, res, next) => {
-  const token = req.cookies.jwt
-  if (token) {
-    jwt.verify(token, jwtSecret, (err, decodedToken) => {
-      if (err) {
-        return res.status(401).json({ message: "Not authorized" })
-      } else {
-        if (decodedToken.role !== "Basic") {
-          return res.status(401).json({ message: "Not authorized" })
-        } else {
-          next()
-        }
-      }
-    })
-  } else {
-    return res
-      .status(401)
-      .json({ message: "Not authorized, token not available" })
-  }
 }
