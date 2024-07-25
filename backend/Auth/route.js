5b2188cbe0aa6deebf94420d1dfe3447d9064a33
Andrei Duque <solomonduque04@gmail.com>
2024-07-25 20:24:49 +0000

    fixed login/signup + user/admin pages + made buttons visible

--- backend/Auth/route.js
+++ backend/Auth/route.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const { register, login, update, deleteUser } = require("./auth");
+const { register, login, update, deleteUser, getUsers } = require("./auth");
 const { adminAuth } = require("../middleware/auth")
 router.route("/update").put(adminAuth, update)
 router.route("/deleteUser").delete(adminAuth, deleteUser)
@@ -9,5 +9,6 @@ module.exports = router
 router.route("/login").post(login);
 router.route("/update").put(update);
 router.route("/deleteUser").delete(deleteUser);
+router.route("/getUsers").get(getUsers);
 
 
