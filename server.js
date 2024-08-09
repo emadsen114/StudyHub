99246fa17dad47277cd5a27b8ff2d6eb7de2e968
Andrei Duque <solomonduque04@gmail.com>
2024-08-09 04:48:45 +0000

    updated user page CSS + filter home page to only display posts, no drafts

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'frontend/views'));
 
 // Route to fetch and display posts on the homepage
-app.get('/homePage.html', async (request, response) => {
+app.get('/homePage', async (request, response) => {
   try {
-    const posts = await Post.find().sort({ createdAt: -1 }); // find( {draft: false}) **ADD this later**
+    const posts = await Post.find({draft: false}).sort({ createdAt: -1 }); // find( {draft: false}) **ADD this later**
     response.render('homePage', { posts });
   } catch (err) {
     console.error('Error fetching posts:', err);
